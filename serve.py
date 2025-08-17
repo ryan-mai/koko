6c681e74ee200f52ed7191ce497fc108641b0a5b
Your NameRyan <inoober2008@gmail.com>
2025-08-17 16:47:40 -0400

    UPDATED README + SUPPORT FOR HOSTING

--- serve.py
+++ serve.py
@@ -1,34 +1,63 @@
-import http.server
-import socketserver
+from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+import argparse
+import webbrowser
 import socket
 import sys
 
-PORT = 8000
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def log_message(self, format, *args):
-        sys.stderr.write("%s - - [%s] %s\n" %
-                         (self.client_address[0],
-                          self.log_date_time_string(),
-                          format%args))
+def find_free_port(start_port: int = 8000) -> int:
+    port = start_port
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(("", port))
+                return port
+            except OSError:
+                port += 1
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def main() -> int:
+    parser = argparse.ArgumentParser(description="Serve the current directory over HTTP")
+    parser.add_argument("--port", type=int, default=8000, help="Port to serve on (default 8000)")
+    parser.add_argument("--no-open", action="store_true", help="Don't open the browser automatically")
+    args = parser.parse_args()
+
+    port = args.port
+
+    if port == 0:
+        port = find_free_port(8000)
+    else:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(("", port))
+            except OSError:
+                print(f"Port {port} is in use, finding a free port...")
+                port = find_free_port(port + 1)
+
+    host = "0.0.0.0"
+    server_address = (host, port)
+
+    handler_class = SimpleHTTPRequestHandler
+    httpd = ThreadingHTTPServer(server_address, handler_class)
+
+    url = f"http://localhost:{port}/"
+
+    print(f"Serving HTTP on {host} port {port} (http://localhost:{port}/) ...")
+
+    if not args.no_open:
+        try:
+            webbrowser.open(url)
+        except Exception:
+            pass
+
     try:
-        # doesn't have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Shutting down server...")
+        httpd.shutdown()
+        return 0
+    return 0
+
 
 if __name__ == "__main__":
-    ip = get_ip()
-    print(f"Serving at http://{ip}:{PORT}/ (accessible on your local network)")
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            print("\nServer stopped.")
+    sys.exit(main())
